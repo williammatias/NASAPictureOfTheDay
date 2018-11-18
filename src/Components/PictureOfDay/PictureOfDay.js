d7a51788883e83e3e5d07b5d1e6d8496758af26f
William Matias <matias.b.willliam@gmail.com>
2018-11-18 02:24:48 -0500

    better css

--- src/Components/PictureOfDay/PictureOfDay.js
+++ src/Components/PictureOfDay/PictureOfDay.js
@@ -1,5 +1,19 @@
-import React, { Component } from 'react';
-import Particles from 'react-particles-js';
+import React, {Component} from 'react';
+import classnames from 'classnames';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import red from '@material-ui/core/colors/red';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 import './PictureOfDay.css';
 
@@ -10,18 +24,70 @@ class PictureOfDay extends Component {
         super(props);
         this.props.loadNASAPictureOfDay();
     }
+    state = { expanded: false };
 
-  render() {
-    return (
-      <div className="App">
-          <img src={this.props.pictureOfDay.hdurl} width={300} height={300}/>
-          <p>
-              {this.props.pictureOfDay.explanation}
-          </p>
-          {/*<Particles />*/}
-      </div>
-    );
-  }
+    handleExpandClick = () => {
+        this.setState(state => ({ expanded: !state.expanded }));
+    };
+
+    render() {
+
+        return (
+            <div className="App">
+                <Card className="card">
+                    <CardHeader
+                        avatar={
+                            <Avatar aria-label="Recipe" >
+                                R
+                            </Avatar>
+                        }
+                        action={
+                            <IconButton>
+                                <MoreVertIcon/>
+                            </IconButton>
+                        }
+                        title="Shrimp and Chorizo Paella"
+                        subheader="September 14, 2016"
+                    />
+                    <CardMedia
+                        image={this.props.pictureOfDay.url}
+                        component="img"
+                        alt="Contemplative Reptile"
+                        title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                        <Typography component="p">
+                            {this.props.pictureOfDay.explanation}
+                        </Typography>
+                    </CardContent>
+                    <CardActions disableActionSpacing>
+                        <IconButton aria-label="Add to favorites">
+                            <FavoriteIcon/>
+                        </IconButton>
+                        <IconButton aria-label="Share">
+                            <ShareIcon/>
+                        </IconButton>
+                        <IconButton
+                            onClick={this.handleExpandClick}
+                            aria-expanded={this.state.expanded}
+                            aria-label="Show more"
+                        >
+                            <ExpandMoreIcon/>
+                        </IconButton>
+                    </CardActions>
+                    <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+                        <CardContent>
+                            <Typography paragraph>
+                                {this.props.pictureOfDay.explanation}
+                            </Typography>
+
+                        </CardContent>
+                    </Collapse>
+                </Card>
+
+            </div>
+        );
+    }
 }
 
 export default PictureOfDay;
